69111e29be03205b82110057a2b2a0379f8fb7b6
EdvinJakobsson <31991380+EdvinJakobsson@users.noreply.github.com>
2018-10-20 14:54:49 +0200

    keras-mlp updated with help of Eric Andersson

--- extraction_functions.py
+++ extraction_functions.py
@@ -2,6 +2,7 @@ from reader import *
 import numpy as np
 import nltk
 from nltk.tokenize import RegexpTokenizer
+import matplotlib.pyplot as plt
 
 
 def extract_length(array, data):    # array is input values, data is the list created by reader.py.
@@ -23,12 +24,16 @@ def extract_length(array, data):    # array is input values, data is the list cr
 
 ##################################################
 
+# ändra så att row[x] är variabel, kalla på funktionen från
+#  andra funktioner, snarare än att kopiera den här 15 ggr
+
+##kolla också fördelningen av betyg!
 
 def extract_domain1_score(array, data):
     try:
         values = []
         for row in data:
-            values.append(row[6])
+            values.append(float(row[6]))
 
         array = np.c_[array, values]
     except:
@@ -41,10 +46,16 @@ def extract_domain1_score(array, data):
 
 #data = read_dataset(10)
 #array = np.zeros((10, 0))
-#array = extract_domain1_score(array, data)
+#array = extract_length(array, data)
 #print(array)
 
 
-
-
+# Display the distribution of grades
+def stats():
+    data = read_dataset(1780)
+    d = np.zeros((1780, 0))
+    d = extract_domain1_score(d, data)
+    plt.hist(d)
+    #plt.yscale("log")
+    plt.show()
 
--- keras.py
+++ keras.py
@@ -3,37 +3,90 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from reader import *
 from extraction_functions import *
+import matplotlib.pyplot as plt
 
 number_of_essays = 1780
-x = np.zeros((number_of_essays,1))
-y = np.zeros((number_of_essays,0))
+x = np.zeros((number_of_essays, 0))
+y = np.zeros((number_of_essays, 0))
 
 data = read_dataset(number_of_essays)
-#x = extract_length(x, data)
+x = extract_length(x, data)
 y = extract_domain1_score(y, data)
 
 x_train = x
 y_train = y
+
+d = np.zeros([number_of_essays, 11])
+for i in range(number_of_essays):
+    d[i, int(y_train[i]-2) ] = 1
+
+y_train = d
 
 x_train, x_test, y_train, y_test = train_test_split(x_train, y_train, test_size=0.20, random_state=415)
 
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
+print(x_train.shape)
 
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(128, activation=tf.nn.relu))
-model.add(tf.keras.layers.Dense(13, activation=tf.nn.softmax))
+#model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(20, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dropout(0.2))
+model.add(tf.keras.layers.Dense(11, activation=tf.nn.softmax))
 
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'] )
-model.fit(x_train, y_train, epochs=10)
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'] )
+estimator = model.fit(x_train, y_train, epochs=40, validation_data=(x_test, y_test))
 
 
-val_loss, val_acc = model.evaluate(x_test, y_test)
-print(val_loss, val_acc)
+
+#####################################
+
+# Evaluate model.
+loss, acc = model.evaluate(x=x_train, y=y_train, batch_size=1, verbose=1, sample_weight=None, steps=None)
+print('Training loss = {}'.format(loss))
+print('Training accuracy = {}'.format(acc))
+
+loss, acc = model.evaluate(x=x_test, y=y_test, batch_size=1, verbose=1, sample_weight=None, steps=None)
+print('Test loss = {}'.format(loss))
+print('Test accuracy = {}'.format(acc))
+
+
+# Plot the training error
+plt.plot(estimator.history['loss'], label='Training')
+plt.plot(estimator.history['val_loss'], label='Validation')
+plt.ylabel('Error')
+plt.xlabel('Epoch')
+plt.legend()
+plt.savefig('loss-function.pdf', bbox_inches='tight')
+plt.show()
+
+# Plot the training error¬
+plt.plot(estimator.history['acc'], label='Training')
+plt.plot(estimator.history['val_acc'], label='Validation')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend()
+plt.savefig('accuracy-function.pdf', bbox_inches='tight')
+plt.show()
+
+
+################################
+
+
+
+
+
+
+
+#val_loss, val_acc = model.evaluate(x_test, y_test)
+#print(val_loss, val_acc)
 
 p = model.predict([x_test])
 
-for i in range(len(p)):
-    print(np.argmax(p[i]))
+#print(p[0])
+
+#for i in range(len(p)):
+#    print(np.argmax(p[i]))
+
+print((np.argmax(p)!=6))
